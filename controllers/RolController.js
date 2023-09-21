ad6f310186378116b11b5efab689cef3eb7da82d
Angel <luisangelacostagonzalez2@gmail.com>
2023-09-21 05:46:31 -0500

    RolByName

--- controllers/RolController.js
+++ controllers/RolController.js
@@ -15,6 +15,14 @@ export class RolController{
         res.json(one)
     }
 
+        static async getOneByName(req, res){
+        const one = await RolModel.getOneByName(req.body.name)
+        if(one===null){
+            return res.status(404).json({message:"Rol no encontrado"})
+        }
+        res.json(one)
+    }
+
     static async create(req, res){
         const createRes = await RolModel.create(req.body)
         if(!createRes){
